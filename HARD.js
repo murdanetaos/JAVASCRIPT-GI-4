a42cc4185e6e32c5afaf297dd27bfad7b8d63bdd
murdanetaos <119532654+murdanetaos@users.noreply.github.com>
2023-05-21 19:23:00 -0400

    NEW CHANGES

--- HARD.js
+++ HARD.js
@@ -1,16 +1,18 @@
-function myFunction()
-{   
-document.body.style.backgroundColor= "red";
+var el_up = document.getElementById("UP");
+var el_down = document.getElementById("DOWN");
+var str = "Click on button to change the background color";
+          
+el_up.innerHTML = str;
+          
+function changeColor(color) {
+    document.body.style.background = color;
 }
-function myFunction1()
-{   
-document.body.style.backgroundColor= "white";
-}
-function myFunction12()
-{   
-document.body.style.backgroundColor= "pink";
-document.body.style.backgroundColor= "yellow";
-document.body.style.backgroundColor= "blue";
-
-
-}
+  
+function red() {
+    changeColor('red');
+    el_down.innerHTML = "Background Color changed";
+}        
+function white() {
+    changeColor('white');
+    el_down.innerHTML = "Background Color changed";
+}        
